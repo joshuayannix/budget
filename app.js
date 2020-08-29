fdb6fb28bd9de34619e19b1e319b3d0257a0b9b8
joshuayannix <joshuayannix@gmail.com>
2020-08-29 15:19:40 -0400

    ui with interpolation

--- app.js
+++ app.js
@@ -55,16 +55,17 @@ var budgetController = (function() {
 
 })();
 
-
-
-
 var UIController = (function() {
+
   var DOMstrings = {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
-  }
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list',
+  };
+
   return {
     getInput: function() {
       return {
@@ -73,6 +74,43 @@ var UIController = (function() {
         value: document.querySelector(DOMstrings.inputValue).value
       };
     },
+
+    addListItem: function(obj, type) {
+      var html, element;
+      // Create HTML string with placeholder text
+      if (type === 'inc') {
+        element = DOMstrings.incomeContainer;
+        html = `
+          <div class="item clearfix" id="income-${obj.id}">
+            <div class="item__description">${obj.description}</div>
+            <div class="right clearfix">
+                <div class="item__value">${obj.value}</div>
+                <div class="item__delete">
+                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                </div>
+            </div>
+          </div>
+        `
+      } else if (type === 'exp') {
+        element = DOMstrings.expensesContainer;
+        html = `
+          <div class="item clearfix" id="expense-${obj.id}">
+            <div class="item__description">${obj.description}</div>
+            <div class="right clearfix">
+                <div class="item__value">${obj.value}</div>
+                <div class="item__percentage">21%</div>
+                <div class="item__delete">
+                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                </div>
+            </div>
+          </div>
+        `
+      }
+
+      // Insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', html);
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -103,10 +141,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     // 2. Add the item to the budget controller
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
     // 3. Add the item to the UI
+    UICtrl.addListItem(newItem, input.type)
+
     // 4. Calculate the budget
     // 5. Display the budget on the UI
-    console.log('item was clicked or enter was pressed')
   };
 
   return {
