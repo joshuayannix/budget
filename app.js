ce84278fc8ebc32d14337529978c5c6e87f5d917
joshuayannix <joshuayannix@gmail.com>
2020-09-03 09:10:15 -0400

    color updates and budget number fix

--- app.js
+++ app.js
@@ -176,6 +176,12 @@ var UIController = (function() {
    return (type === "exp" ? "-" : "+") + " " + "$" + int + "." + dec;
   };
 
+  var nodeListForEach = (list, callback) => {
+    for (let i=0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  }
+
   return {
     getInput: function() {
       return {
@@ -238,7 +244,7 @@ var UIController = (function() {
 
     displayBudget: function(obj) {
       let type;
-      obj.budget > 0 ? type = 'inc' : 'exp';
+      obj.budget >= 0 ? type = 'inc' : type = 'exp';
 
       document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
       document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
@@ -253,12 +259,6 @@ var UIController = (function() {
 
     displayPercentages: function(percentages) {
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
-
-      var nodeListForEach = (list, callback) => {
-        for (let i=0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      }
 
       nodeListForEach(fields, function(current, index) {
         if (percentages[index] > 0) {
@@ -278,6 +278,21 @@ var UIController = (function() {
       month = now.getMonth();
 
       document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+    },
+
+    changedType: function() {
+
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType + ',' +
+        DOMstrings.inputDescription + ',' +
+        DOMstrings.inputValue
+      );
+
+      nodeListForEach(fields, cur => {
+        cur.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
     },
 
     getDOMstrings: function() {
@@ -304,6 +319,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
   };
 
